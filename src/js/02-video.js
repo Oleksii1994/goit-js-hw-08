9b5f1200a6f3cdc763c097a69ec6c3765e38267a
Oleksii Kochurov <aleksei.kochuroff@icloud.com>
2023-02-13 15:07:20 +0200

    task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const onPlay = function (data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+
+  const savedData = localStorage.getItem('videoplayer-current-time');
+  const parsedData = JSON.parse(savedData);
+  return parsedData;
+};
+
+function getterDataLocalStorage() {
+  const dataFromLocalStorage = JSON.parse(
+    localStorage.getItem('videoplayer-current-time')
+  );
+
+  if (dataFromLocalStorage) {
+    return dataFromLocalStorage;
+  }
+}
+player.setCurrentTime(getterDataLocalStorage());
+player.on('timeupdate', throttle(onPlay, 1000));
